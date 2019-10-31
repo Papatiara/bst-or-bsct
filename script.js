b2d4111c7546642f63cf5f6d892a95e05f07bcd4
Patricia Shirazi <pshirazi@google.com>
2019-10-31 10:55:02 -0700

    made syntax more consistent and added removedNode function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let nodeElement = (input) => {
-  let result = document.createElement("div");
-  result.textContent = input;
-  result.classList.add("root");
-  result.left = null
-  result.right = null
-  return result
+const NODE_CLASSNAME = "nodeStyle";
+
+const nodeElement = (input) => {
+  const element = document.createElement("div");
+  element.textContent = input;
+  element.classList.add(NODE_CLASSNAME);
+  element.left = null;
+  element.right = null;
+  return element;
 }
 
 
@@ -14,39 +16,39 @@ class BinarySearchTree {
   }
   
   insert(input) {
-    let newNode = nodeElement(input);     
+    const newNode = nodeElement(input);     
     if (this.root === null) {
       this.root = newNode
-      document.body.appendChild(newNode)  
+      document.body.appendChild(newNode); 
     } else {
-      this.insertNode(this.root,newNode)
+      this.insertNode(this.root,newNode);
     }
   }
     
   insertNode(node, newNode) {
     let nodeValue = node.textContent
        if (nodeValue.includes('*')){
-        nodeValue = nodeValue.split('*')[0]
+        nodeValue = nodeValue.split('*')[0];
      }
    
-    let edge = document.createElement("div");
+    const edge = document.createElement("div");
     if (Number(newNode.textContent) > Number(nodeValue)) {
         if(node.left === null) {
           edge.classList.add("edge-left")
           edge.textContent = '*';
-          node.appendChild(edge)
+          node.appendChild(edge);
           node.left = newNode;
-          edge.appendChild(newNode)
+          edge.appendChild(newNode);
         } else {
-          this.insertNode(node.left, newNode)
+          this.insertNode(node.left, newNode);
         }
     } else if (Number(newNode.textContent) < Number(nodeValue)) {
          if(node.right === null) {
-          edge.classList.add("edge-right")
+          edge.classList.add("edge-right");
           edge.textContent = '*';
           node.appendChild(edge)
           node.right = newNode;
-          edge.appendChild(newNode)
+          edge.appendChild(newNode);
         } else {
           this.insertNode(node.right, newNode)
         }
@@ -60,51 +62,56 @@ class BinarySearchTree {
   removeNode(node, value) {
     if (node === null) {
       return null;
-    } else if (Number(value) > Number(node.textContent)) {
-        node.left = this.removeNode(node.left, value);
-        return node
-    } else if (Number(value) < Number(node.textContent)) {
-        node.right = this.removeNode(node.right, value);
+    } 
+    const formatingNodeValue = node.textContent.replace(/ /g,'').split('*');
+    const nodeRootValue = formatingNodeValue[0]
+    if (Number(value) > Number(nodeRootValue)) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (Number(value) < Number(nodeRootValue)) {
+      node.right = this.removeNode(node.right, value);
+      return node;
     } else {
-      if (node.left === null && node.right === null)  {
-        node = null;
-        return node;
-      } if (node.left === null) {
-        node = node.right;
-        return node;
-      } else if (node.right === null) {
-        node = node.left;
-        return node;
+      if (node.left === null && node.right === null) {
+        node.remove();
+        return node;      
+      } if (node.left) {
+         node.textContent = node.left.textContent;
+         return node;
+      } else if (node.right) {
+         node.textContent = node.right.textContent;
+         return node;
       }
+    
     }
   }
 }
 
 
-let class2 = new BinarySearchTree()
+const tree = new BinarySearchTree()
 
-let addValue = document.getElementById("valueAdded").addEventListener("click", () => {
-  let input = document.getElementById("value").value;
-  let warning = document.createElement('div')
-  warning.classList.add("warning")
-  if (Number.isNaN(Number(input)) || input === "") {
+const addingNode = document.getElementById("valueAdded").addEventListener("click", () => {
+  const toBeAdded = document.getElementById("value").value;
+  const warning = document.createElement('div');
+  warning.classList.add("warning");
+  if (Number.isNaN(Number(toBeAdded)) || toBeAdded === "") {
     warning.textContent = 'the value should be an integer'
-    document.body.appendChild(warning)
+    document.body.appendChild(warning);
   } else {
-    class2.insert(input)
+    tree.insert(toBeAdded);
   }
   document.getElementById("value").value = ' ';
 });
 
-let removeValue = document.getElementById("valueRemoved").addEventListener("click", () => {
-  let removed = document.getElementById("value").value
-  document.getElementById("value").value = ' '
-  class2.remove(removed)
+const removingNode = document.getElementById("valueRemoved").addEventListener("click", () => {
+  const toBeRemoved = document.getElementById("value").value;
+  document.getElementById("value").value = ' ';
+  tree.remove(toBeRemoved);
 });
 
 
-let removeWarning = document.getElementById("value").addEventListener("click", () => {
+const removeWarning = document.getElementById("value").addEventListener("click", () => {
   if (document.getElementsByClassName("warning").length > 0) {
-    document.body.removeChild(document.getElementsByClassName("warning")[0])
+    document.body.removeChild(document.getElementsByClassName("warning")[0]);
   }
 });
